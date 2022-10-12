a1598cdbed5932a4c60ce553f9200009858ea691
Come Vitis <business.vitis@gmail.com>
2022-10-12 21:42:36 +0800

    Adding check for installed package on the VM

--- kubernetes/script.sh
+++ kubernetes/script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 CONTROL_PLANE_TOKEN="K10db85acbbade8b2e7df2057aa8c51eaf60964fe2703c174064df0b0b6becb8d87::server:eb6d552feec9d8fd9b28ca70d206c1a2" # Master node token
-MASTER_NODE_NAME="control-plane-k3s"
+CONTROL_NODE="control-plane-k3s"
 
 # Check multipass binary
 if [ -x "$(command -v multipass.exe)" > /dev/null 2>&1 ]; then
@@ -14,18 +14,39 @@ else
     exit 1
 fi
 
-if ! $(multipass list | grep -q $MASTER_NODE_NAME) ; then
+if ! $(multipass list | grep -q $CONTROL_NODE) ; then
     echo "The control plane VM does not exit ... Creating it..."
-    multipass launch --name $MASTER_NODE_NAME --cpus 2 --mem 2048M --disk 5G focal
+    multipass launch --name $CONTROL_NODE --cpus 2 --mem 2048M --disk 5G focal
 else
     echo "The control plane VM is found ..."
 fi
 
 echo "Launching the control plane VM ..."
-multipass start $MASTER_NODE_NAME
+multipass start $CONTROL_NODE
 
-while [[ $(multipass exec $MASTER_NODE_NAME sudo kubectl get nodes  --no-header 2>/dev/null | grep -c -v "not found") ]];
-do 
-    echo "Waiting for $MASTER_NODE_NAME to start..."
-    sleep 2;
-done
+#while [[ $(multipass exec $CONTROL_NODE sudo kubectl get nodes  --no-header 2>/dev/null | grep -c -v "not found") ]];
+#do 
+#    echo "Waiting for $CONTROL_NODE to start..."
+#    sleep 2;
+#done
+
+#while [[ $(multipass list | grep control-plane-k3s | awk '{print $2}' | grep -q 'Stopped') ]]; 
+#do
+#    echo "Waiting for $CONTROL_NODE to start..."
+#    sleep 5
+#done
+
+if ! $(multipass exec control-plane-k3s echo "$(command -v apt)" | grep -q 'apt'); then
+    echo "Missing apt package, installing..."
+    multipass exec $CONTROL_NODE sudo apt update
+    multipass exec $CONTROL_NODE sudo apt upgrade
+else
+    echo "apt package already installed..."
+fi
+
+if ! $(multipass exec control-plane-k3s echo "$(command -v kubectl)" | grep -q 'kubectl'); then
+    echo "Missing k3s package, installing..."
+    multipass exec $CONTROL_NODE sudo curl -sfL https://get.k3s.io | sh -
+else
+    echo "k3s package already installed..."
+fi
