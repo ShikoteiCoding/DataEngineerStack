72cd64dfa28fbb4314cf79eae508aaf3a77a4829
Come <56791111+ShikoteiCoding@users.noreply.github.com>
2023-04-09 16:43:39 +0800

    Testing compute twitter 3

--- spark/tests/twitter/test_job_twitter_data_3.py
+++ spark/tests/twitter/test_job_twitter_data_3.py
@@ -5,11 +5,16 @@ from pyspark.sql import SparkSession, DataFrame
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
 
-from jobs.twitter.job_twitter_data_3 import tweet_rank_per_day_per_user
-from jobs.common import cast_column, attach_column
+from jobs.twitter.job_twitter_data_3 import (
+    tweet_rank_per_day_per_user,
+    join_cond_screen_name_over_the_day,
+)
+from jobs.common import cast_column, attach_column, join_dataframe
 
 
-def test_tweet_rank_per_day_per_user(spark: SparkSession, tweet_test_df: DataFrame):
+def test_tweet_rank_per_day_per_user(
+    spark: SparkSession, tweet_test_df: DataFrame
+) -> None:
     """test compute rank or tweet per day per user"""
 
     output = (
@@ -30,4 +35,43 @@ def test_tweet_rank_per_day_per_user(spark: SparkSession, tweet_test_df: DataFra
         (1,),
     ]
 
-    assert output == spark.createDataFrame(expected_output, ["rank_screen_name"]).collect()
+    assert (
+        output == spark.createDataFrame(expected_output, ["rank_screen_name"]).collect()
+    )
+
+
+def test_join_cond_screen_name_over_the_day(
+    spark: SparkSession, tweet_test_df: DataFrame
+) -> None:
+    """test join condition to differentiate screen name change"""
+    columns = [
+        "left.user_id",
+        "left.screen_name",
+        "right.screen_name",
+        "right.tweet_date",
+    ]
+
+    output = attach_column(
+        tweet_test_df, "rank_screen_name", tweet_rank_per_day_per_user
+    )
+
+    # join dataframe
+    output = (
+        join_dataframe(
+            output.alias("left"),
+            output.alias("right"),
+            join_cond_screen_name_over_the_day,
+            join_type="inner",
+        )
+        .select(columns)
+        .collect()
+    )
+
+    expected_output = [
+        (1, "Alice", "Alice2", "2022-01-01"),
+        (2, "Charlie", "Charlie2", "2022-01-01"),
+        (3, "David", "David2", "2022-01-01"),
+        (3, "David2", "David3", "2022-01-01"),
+    ]
+
+    assert output == spark.createDataFrame(expected_output, columns).collect()
