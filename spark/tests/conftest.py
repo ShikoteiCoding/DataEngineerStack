d5450d8c8f10c60fcd1c533d3a4762892c32fc73
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2023-08-13 13:56:50 +0800

    spark: clean spark test builder

--- spark/tests/conftest.py
+++ spark/tests/conftest.py
@@ -11,10 +11,8 @@ from typing import Generator
 @pytest.fixture(scope="session")
 def spark() -> Generator[SparkSession, None, None]:
     spark = (
-        SparkSession.builder.master("local[1]")  # type: ignore
+        SparkSession.builder.master("local[1]")
         .appName("local-tests")
-        .config("spark.executor.cores", "1")
-        .config("spark.executor.instances", "1")
         .config("spark.sql.shuffle.partitions", "1")
         .config("spark.driver.bindAddress", "127.0.0.1")
         .getOrCreate()
