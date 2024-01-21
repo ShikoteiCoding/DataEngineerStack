6ab32ade8ed71bd0195095a54c64650559977302
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2024-01-21 22:47:30 +0800

    generator: send messages to local kafka

--- microservices/generator/definition.py
+++ microservices/generator/definition.py
@@ -2,10 +2,9 @@ from dataclasses import dataclass, field
 
 from pathlib import Path
 from ruamel.yaml import YAML
-from collections import OrderedDict
 
 
-def parse_definition_config(path: str):
+def parse_definition_config(path: str) -> dict:
     """Load definition to build generator."""
     definition = YAML().load(Path(path))
 
--- microservices/generator/entrypoint.py
+++ microservices/generator/entrypoint.py
@@ -38,8 +38,10 @@ class KafkaSink(Sink):
         print(f"connecting to kafka with conf {conf}...")
         self.producer: Producer = Producer(conf)
 
-    def post(self, producer: ProducerMessage, topic: str = ""):
+    def post(self, producer: ProducerMessage, topic: str = "test"):
+        print("i'm posting")
         self.producer.produce(topic, key="key", value="value", callback=acked)
+        self.producer.poll(1)
 
 
 class ConsoleSink(Sink):
@@ -53,14 +55,14 @@ class ConsoleSink(Sink):
 class Config(dict):
     def __init__(self, config: dict):
         super().__init__(config)
-    
+
     @property
     def kafka_conf(self) -> dict:
         conf = {
             "bootstrap.servers": self.get("KAFKA_BOOTSTRAP_SERVERS"),
         }
         return conf
-    
+
     def __getitem__(self, __key: Any, __default: Any = None) -> Any:
         return self.get(__key, __default)
 
